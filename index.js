7aae0c51394dfd835d9ab202949a6cb2fffafa59
Adrien Burgun <adrien.burgun@orange.fr>
2022-01-17 17:40:07 +0100

    :sparkles: findSuspiciousLink

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const check = require("./lib/check");
 const list = require("./lib/list");
 
 exports.checkMessage = check.checkMessage;
+exports.findSuspiciousLink = check.findSuspiciousLink;
 
 exports.listDomains = list.listDomains;
